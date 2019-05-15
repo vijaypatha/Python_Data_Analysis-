b50a55947cc522cbb1f6b9d61fca19df49a2b962
Patha <vipatha@amazon.com>
2019-05-15 10:07:33 -0700

    series

--- Start_with_Pandas/pandas_v1.py
+++ Start_with_Pandas/pandas_v1.py
@@ -2,3 +2,21 @@ import pandas as pd
 from pandas import Series, DataFrame
 #Series and DataFrame are the workhorse of pandas 
 
+#Lets start with: Series([], index =[]). Series is a arry like object 
+
+# CREATING PANDAS SERIES 
+pathas = pd.Series(["vijay", "heidi", "heidi", 3])
+print(pathas) # when i print a Series, series automatically provides the index number
+
+# HOW TO ACCESS AN ELEMENT IN PANDAS 
+print(pathas[0]) #very similar to accessing an element from a list 
+
+# HOW TO CHANGES THE INDEX of SERIES 
+pathas = pd.Series(["vijay", "heidi", "heidi", 3], index = ["worst", "the best", "awesome baby", "random"])
+print(pathas) #pretty straight forward to change the default index of a series 
+
+
+
+
+
+
