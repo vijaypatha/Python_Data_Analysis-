8cdd709926560f6d829b61a51b428736a132d9f8
Patha <vipatha@amazon.com>
2019-05-16 08:16:16 -0700

    filters

--- Start_with_Pandas/pandas_v1.py
+++ Start_with_Pandas/pandas_v1.py
@@ -15,6 +15,14 @@ print(pathas[0]) #very similar to accessing an element from a list
 pathas = pd.Series(["vijay", "heidi", "heidi", 3], index = ["worst", "the best", "awesome baby", "random"])
 print(pathas) #pretty straight forward to change the default index of a series 
 
+#HOW TO FILTER PANDAS 
+ages = [10,20,30,40]
+pets_names = ["mickey", "rosie", "kitty", "rufus"]
+my_data = pd.Series(ages, index = pets_names)
+print(my_data) #expected an index of names and column of ages
+
+multiply_series = my_data * 2
+print(multiply_series)
 
 
 
