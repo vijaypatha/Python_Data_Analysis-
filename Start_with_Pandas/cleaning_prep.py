b3cecb90c0989a166d6040646b4ae991c28afb9d
Patha <vipatha@amazon.com>
2019-05-18 12:19:28 -0700

    ind

--- Start_with_Pandas/cleaning_prep.py
+++ Start_with_Pandas/cleaning_prep.py
@@ -6,6 +6,6 @@ import seaborn as sb
 import scipy as sp
 
 
-raw_df = pd.read_csv("C:/Users/vipatha/Documents/2. 2019_DeepDives/Softlines_DeepDive/DD-Analysis/All_Softlines_CPLF/softlines_neg.csv",encoding = "ISO-8859-1")
+raw_df = pd.read_csv("..")
 
 raw_df.head()
